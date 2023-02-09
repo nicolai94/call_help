50b731ba105916d5d62e526d1cbfa14f6718fa8b
Nicolai94 <nicolas9441@gmail.com>
2023-02-09 11:24:41 +0300

    fix: group and employee serializers and views

--- common/serializers/mixins.py
+++ common/serializers/mixins.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from users.serializers.nested.users import UserShortSerializer
+
 
 class ExtendedModelSerializer(serializers.ModelSerializer):  # миксин сериализатора
     class Meta:
@@ -9,3 +11,11 @@ class ExtendedModelSerializer(serializers.ModelSerializer):  # миксин се
 class DictMixinSerializer(serializers.Serializer):  # миксин для статусов
     code = serializers.CharField()
     name = serializers.CharField()
+
+
+class InfoModelSerializer(ExtendedModelSerializer):
+    created_by = UserShortSerializer()
+    updated_by = UserShortSerializer()
+
+    class Meta:
+        abstract = True
